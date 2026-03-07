338c8480f147de3e19cf1c706ac0a4c76413f6a1
Nicola Margolis <nicolamargolis@Nicolas-MacBook-Air.local>
2026-03-07 16:27:15 -0600

    Added Ability to Save to File

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // This was all a test to get more practice in rust!
 // The website where this code was found at is below
 // https://dev.to/abhishek2010devsingh/ascii-art-code-in-rust-48hn#:~:text=Full%20code,%7B%20for%20x%20in%200..
-// I take no credit in making any of this, all I added was more comments based on what the article explained
+// The only credit I take in making this is adding the ability so save to a file
 use clap::Parser;
 use image::{DynamicImage, GenericImageView};
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,6 +16,9 @@ pub struct ImageValue{
     // Scale factor for ASCII art (Default is 3)
     #[arg(short, long, default_value_t = 3)]
     scale: u32,
+
+    #[arg(short,long)]
+    file: bool,
 }
 
 fn get_str_ascii(intent: u8) -> &'static str {
@@ -23,11 +27,20 @@ fn get_str_ascii(intent: u8) -> &'static str {
     return ascii[index as usize];
 }
 
-fn get_image(dir: &str, scale: u32){
+fn save_image(text: String){
+    let file_path = "output.txt";
+    let contents: String = text;
+
+    // TODO: More error handling
+    fs::write(file_path, contents).unwrap();
+}
+
+fn get_image(dir: &str, scale: u32, save: bool){
     // Opens the image at the path specified
     // TODO: .unwrap() panics if the image isn't there, maybe make a proper error system
     let img: DynamicImage = image::open(dir).unwrap();
     let (width, height) = img.dimensions();
+    let mut total_text: String = String::new();
     for y in 0..height{
         for x in 0..width{
             // Checks if the pixel should be sampled based on current image dimensions
@@ -40,17 +53,32 @@ fn get_image(dir: &str, scale: u32){
                 if pix[3] == 0{
                     intent = 0;
                 }
-               // Prints the ASCII charecter based on the given intentsity value
-               print!("{}", get_str_ascii(intent));
+                if save == false{
+                    // Prints the ASCII charecter based on the given intentsity value
+                    print!("{}", get_str_ascii(intent));
+                }
+                else{
+                    total_text.push_str(get_str_ascii(intent));
+                }
             }
         }
         // Prints a new line every scale * 2 rows to make sure everything lines up good
         if y % (scale * 2) == 0{
-            println!("");
+            if save == false{
+                println!("");
+            }
+            else{
+                total_text.push_str("\n");
+            }
         }
     }
+
+    if save == true{
+        save_image(total_text);
+    }
+
 }
 fn main() {
     let value = ImageValue::parse();
-    get_image(&value.path, value.scale);
+    get_image(&value.path, value.scale, value.file);
 }
